81e81f7950677064c9f97d4d598a4fc46cc89733
Yogesh Suthar <ysuthar.web@gmail.com>
2019-09-17 12:48:11 +0530

    Minor Fixes and animation updates

--- src/script.js
+++ src/script.js
@@ -1,15 +1,15 @@
 if($('.cust-tabs').length){
 	$('.cust-tabs').each(function(){
 	  	var thisElem = $(this),
-          thisElemCont = thisElem.find('.tab-sect').children(),
-          activeTab = $(this).index();
-    
+          thisElemCont = thisElem.find('.tab-sect').children();
+   
 		  thisElemCont.hide();
 		  thisElem.find('ul.tab-links li').click(function(index){
+        var activeTab = $(this).index();
          if(!$(this).hasClass('active')){
               thisElem.find('ul.tab-links li').removeClass('active');
               $(this).addClass('active');
-              thisElem.find('.tab-cont').stop().animate({"opacity":0},500, function(){
+              thisElem.find('.tab-cont').stop().animate({"opacity":0},400, function(){
                 thisElem.find('.tab-cont').stop().hide().css({"opacity":1});
                 $($(thisElemCont)[activeTab]).stop().fadeIn();
               })
